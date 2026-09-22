cb02072ef3813b6b7f886b08c00616ac61bfdb69
sundeep <sundeep@sundeeps-MacBook-Air.local>
2026-09-22 10:43:29 +0530

    Switch to light obfuscation to bypass SW Blob constraints

--- public/main.js
+++ public/main.js
@@ -216,7 +216,7 @@ window.onload = function() {
         symbol: urlLayoutId ? undefined : urlSymbol,
         interval: urlLayoutId ? undefined : urlInterval,
         container: 'tv_chart_container',
-        library_path: 'charting_library/',
+        library_path: 'chart_engine/',
         datafeed: Datafeed,
         save_load_adapter: localSaveLoadAdapter,
         locale: 'en',
